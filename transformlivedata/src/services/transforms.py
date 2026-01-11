e3c1c7d8529f7fd5bf3a02f3fb2b56f5831c7bdd
andremoreirafocus <andre.moreira@focus-sys.com.br>
2026-01-11 09:53:00 -0300

    transformlivedata/feat: made table_name a parameter when saving records to db

--- transformlivedata/src/services/transforms.py
+++ transformlivedata/src/services/transforms.py
@@ -11,6 +11,20 @@ from psycopg2.extras import execute_values
 logger = logging.getLogger(__name__)
 
 
+def transform_position(source_bucket, app_folder, table_name):
+    logger.info("Transforming position...")
+    raw_positions = load_positions(source_bucket, app_folder)
+    if not raw_positions:
+        logger.error("No position data found to transform.")
+        return
+    positions_table = get_positions_table_from_raw(raw_positions)
+    if not positions_table:
+        logger.error("No valid position records found after transformation.")
+        return
+    save_positions_to_db(positions_table, table_name)
+    logger.info("Positions transformed successfully.")
+
+
 def load_positions(source_bucket, app_folder):
     """
     Load position data from source bucket and app folder.
@@ -33,9 +47,62 @@ def load_positions(source_bucket, app_folder):
     object_name = f"{prefix}{base_file_name}-{year}{month}{day}{hour_minute}.json"
     datastr = read_file_from_minio(connection_data, source_bucket, object_name)
     logger.info(f"Loaded {len(datastr)} bytes from {object_name}")
-    # print(data)
+    # logger.info(data)
     data = json.loads(datastr)
     return data
+
+
+def get_positions_table_from_raw(raw_positions):
+    def get_record_from_raw(vehicle, line, metadata):
+        vehicle_record = (
+            metadata.get("extracted_at"),  # timestamp_extracao
+            int(vehicle.get("p")),  # veiculo_id
+            line.get("c"),  # linha_lt
+            int(line.get("cl")),  # linha_code
+            int(line.get("sl")),  # linha_sentido
+            line.get("lt0"),  # lt_destino
+            line.get("lt1"),  # lt_origem
+            int(vehicle.get("p")),  # veiculo_prefixo
+            bool(vehicle.get("a")),  # veiculo_acessivel
+            parser.parse(vehicle.get("ta")),  # veiculo_ts
+            float(vehicle.get("py")),  # veiculo_lat
+            float(vehicle.get("px")),  # veiculo_long
+        )
+        return vehicle_record
+
+    logger.info("Converting raw positions to positions table...")
+    positions_table = []
+    if not data_structure_is_valid(raw_positions):
+        logger.error("Raw positions data structure is invalid.")
+        return None
+    payload = raw_positions.get("payload")
+    metadata = raw_positions.get("metadata")
+    if "hr" not in payload:
+        logger.error("No 'hr' field found in raw positions data.")
+        return None
+    if "l" not in payload:
+        logger.error("No 'l' field found in raw positions data.")
+        return None
+    for line in payload["l"]:
+        number_of_vehicles = 0
+        for vehicle in line.get("vs", []):
+            vehicle_record = get_record_from_raw(vehicle, line, metadata)
+            number_of_vehicles += 1
+            positions_table.append(vehicle_record)
+        if number_of_vehicles != int(line.get("qv")):
+            logger.warning(
+                f"Expected {line.get('q', 0)} vehicles for line {line.get('qv')}, but found {number_of_vehicles}."
+            )
+        else:
+            if number_of_vehicles % 1000 == 0:
+                logger.info(
+                    f"Processed {number_of_vehicles} vehicles for line {line.get('qv')}."
+                )
+        if number_of_vehicles % 1000 != 0:
+            logger.info(
+                f"Processed {number_of_vehicles} vehicles for line {line.get('qv')}."
+            )
+    return positions_table
 
 
 def data_structure_is_valid(data):
@@ -75,88 +142,19 @@ def data_structure_is_valid(data):
     return True
 
 
-def get_positions_table_from_raw(raw_positions):
-    positions_table = []
-    if not data_structure_is_valid(raw_positions):
-        logger.error("Raw positions data structure is invalid.")
-        return None
-    payload = raw_positions.get("payload")
-    metadata = raw_positions.get("metadata")
-    if "hr" not in payload:
-        logger.error("No 'hr' field found in raw positions data.")
-        return None
-    if "l" not in payload:
-        logger.error("No 'l' field found in raw positions data.")
-        return None
-    for line in payload["l"]:
-        # print(line)
-        number_of_vehicles = 0
-        for vehicle in line.get("vs", []):
-            # print(vehicle)
-            # vehicle_record = {
-            #     "veiculo_id": vehicle.get("p"),
-            #     # "hora": payload.get("hr"),
-            #     "timestamp_extracao": metadata.get("extracted_at"),
-            #     "linha_lt": line.get("c"),
-            #     "linha_code": line.get("cl"),
-            #     "linha_sentido": line.get("sl"),
-            #     "lt_destino": line.get("lt0"),
-            #     "lt_origem": line.get("lt1"),
-            #     "veiculo_prefixo": vehicle.get("p"),
-            #     "veiculo_acessivel": vehicle.get("a"),
-            #     "veiculo_ts": vehicle.get("ta"),
-            #     "veiculo_lat": vehicle.get("py"),
-            #     "veiculo_long": vehicle.get("px"),
-            # }
-            vehicle_record = (
-                metadata.get("extracted_at"),  # timestamp_extracao
-                int(vehicle.get("p")),  # veiculo_id
-                line.get("c"),  # linha_lt
-                int(line.get("cl")),  # linha_code
-                int(line.get("sl")),  # linha_sentido
-                line.get("lt0"),  # lt_destino
-                line.get("lt1"),  # lt_origem
-                int(vehicle.get("p")),  # veiculo_prefixo
-                bool(vehicle.get("a")),  # veiculo_acessivel
-                parser.parse(vehicle.get("ta")),  # veiculo_ts
-                float(vehicle.get("py")),  # veiculo_lat
-                float(vehicle.get("px")),  # veiculo_long
-            )
-            print(vehicle_record)
-            number_of_vehicles += 1
-            positions_table.append(vehicle_record)
-        if number_of_vehicles != int(line.get("qv")):
-            logger.warning(
-                f"Expected {line.get('q', 0)} vehicles for line {line.get('qv')}, but found {number_of_vehicles}."
-            )
-        else:
-            logger.info(
-                f"Processed {number_of_vehicles} vehicles for line {line.get('qv')}."
-            )
-    return positions_table
-
-
-def transform_position(source_bucket, app_folder, table_name):
-    logger.info("Transforming position...")
-    raw_positions = load_positions(source_bucket, app_folder)
-    if not raw_positions:
-        logger.error("No position data found to transform.")
-        return
-    positions_table = get_positions_table_from_raw(raw_positions)
-    if not positions_table:
-        logger.error("No valid position records found after transformation.")
+def save_positions_to_db(positions_table, table_name):
     """
     Insert 10k+ items from memory list.
-    Assumes list format: (timestamp_extracao, veiculo_id, linha_lt, linha_code, 
-                          linha_sentido, lt_destino, lt_origem, veiculo_prefixo, 
+    Assumes list format: (timestamp_extracao, veiculo_id, linha_lt, linha_code,
+                          linha_sentido, lt_destino, lt_origem, veiculo_prefixo,
                           veiculo_acessivel, veiculo_ts, veiculo_lat, veiculo_long)
     """
     conn = get_db_connection()
 
     cur = conn.cursor()
 
-    insert_sql = """
-    INSERT INTO trusted.posicoes (
+    insert_sql = f"""
+    INSERT INTO {table_name} (
         timestamp_extracao, veiculo_id, linha_lt, linha_code, linha_sentido,
         lt_destino, lt_origem, veiculo_prefixo, veiculo_acessivel, veiculo_ts,
         veiculo_lat, veiculo_long
@@ -167,19 +165,3 @@ def transform_position(source_bucket, app_folder, table_name):
     conn.commit()
     cur.close()
     conn.close()
-    # return
-    # TRANSFORMATION_SQL = """
-    # INSERT INTO trusted.my_fact_table (col1, col2, col3)
-    # SELECT
-    #     col1,
-    #     col2,
-    #     col3
-    # FROM staging.my_raw_table
-    # WHERE ingestion_date = %(ingestion_date)s;
-    # """
-    # ingestion_date = "2024-01-01"  # Example ingestion date
-    # params = {"ingestion_date": ingestion_date}
-    # with db_cursor() as cur:
-    #     cur.execute(TRANSFORMATION_SQL, params)  # execute arbitrary SQL via psycopg2.
-
-    print("Position transformed successfully.")
