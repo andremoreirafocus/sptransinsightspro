922af1b84d8dbb095197792b3efb62f76df97d7f
andremoreirafocus <andre.moreira@focus-sys.com.br>
2026-02-05 17:35:28 -0300

    updatelatestpositions/refactor: decoupled database write function

--- dags-dev/updatelatestpositions/config.py
+++ dags-dev/updatelatestpositions/config.py
@@ -1,10 +1,25 @@
-from dotenv import dotenv_values
+import os
 
 
 def get_config():
-    """
-    Load configuration from .env file using dotenv_values.
-    Returns a dictionary with configuration values.
-    """
+    # Check if we are running inside Airflow
+    if os.getenv("AIRFLOW_HOME"):
+        # Pulling from Airflow Variables
+        # from airflow.models import Variable
+        config = {
+            "RAW_BUCKET": "raw",
+            "TRUSTED_BUCKET": "trusted",
+            "GTFS_FOLDER": "gtfs",
+            "APP_FOLDER": "sptrans",
+            "POSITIONS_TABLE_NAME": "positions",
+            "TRIP_DETAILS_TABLE_NAME": "trip_details",
+            "MINIO_ENDPOINT": "minio:9000",
+            "ACCESS_KEY": "datalake",
+            "SECRET_KEY": "datalake",
+        }
+        return config
+    else:
+        # Pulling from local .env or hardcoded defaults for testing
+        from dotenv import dotenv_values
 
-    return dotenv_values("updatelatestpositions/.env")
+        return dotenv_values("updatelatestpositions/.env")
--- dags-dev/updatelatestpositions/services/create_latest_positions.py
+++ dags-dev/updatelatestpositions/services/create_latest_positions.py
@@ -61,6 +61,34 @@ def get_latest_path(config):
     return latest_file_path
 
 
+def save_to_db(config, latest_positions_df):
+    def get_config(config):
+        try:
+            latest_positions_table_name = config["LATEST_POSITIONS_TABLE_NAME"]
+            host = config["DB_HOST"]
+            port = config["DB_PORT"]
+            dbname = config["DB_DATABASE"]
+            dbuser = config["DB_USER"]
+            password = config["DB_PASSWORD"]
+            return (latest_positions_table_name, host, port, dbname, dbuser, password)
+        except KeyError as e:
+            logger.error(f"Missing required configuration key: {e}")
+            raise
+
+    (latest_positions_table_name, host, port, dbname, dbuser, password) = get_config(
+        config
+    )
+    db_uri = f"postgresql://{dbuser}:{password}@{host}:{port}/{dbname}"
+    engine = create_engine(db_uri)
+    latest_positions_df.to_sql(
+        name=latest_positions_table_name,
+        con=engine,
+        schema="refined",
+        if_exists="replace",
+        index=False,
+    )
+
+
 def create_latest_positions_table(config):
     """
     Refined Layer (Cost-Optimized):
@@ -113,15 +141,7 @@ def create_latest_positions_table(config):
         total_records = refined_df.shape[0]
         logger.info(f"Saving {total_records} at table {latest_positions_table_name}...")
         # 3. POSTGRES: Load
-        db_uri = f"postgresql://{config['DB_USER']}:{config['DB_PASSWORD']}@{config['DB_HOST']}:{config['DB_PORT']}/{config['DB_DATABASE']}"
-        engine = create_engine(db_uri)
-        refined_df.to_sql(
-            name=latest_positions_table_name,
-            con=engine,
-            schema="refined",
-            if_exists="replace",
-            index=False,
-        )
+        save_to_db(config, refined_df)
     except Exception as e:
         logger.error(f"Update failed: {e}")
         raise
