0c1c7282d71d3d8db9b8b80946205c991f7d06b3
andremoreirafocus <andre.moreira@focus-sys.com.br>
2026-03-13 16:11:16 -0300

    feat(transformlivedata): updated airflow to restructured config

--- dags-dev/transformlivedata/config/config.py
+++ dags-dev/transformlivedata/config/config.py
@@ -30,18 +30,10 @@ def get_airflow_config():
     data_expectations = Variable.get(
         "transformlivedata_data_expectations", deserialize_json=True
     )
-    app_folder = general_vars["app_folder"]
-    gtfs_folder = general_vars["gtfs_folder"]
-    raw_bucket = general_vars["raw_bucket"]
-    trusted_bucket = general_vars["trusted_bucket"]
-    positions_table_name = general_vars["positions_table_name"]
-    trip_details_table_name = general_vars["trip_details_table_name"]
-    raw_data_compression = general_vars["raw_data_compression"]
-    raw_data_compression_extension = general_vars["raw_data_compression_extension"]
-    raw_events_table_name = general_vars["raw_events_table_name"]
-    metadata_bucket = general_vars["metadata_bucket"]
-    quality_report_folder = general_vars["quality_report_folder"]
-    quarantined_bucket = general_vars["quarantined_bucket"]
+    storage = general_vars["storage"]
+    tables = general_vars["tables"]
+    compression = general_vars["compression"]
+    quality = general_vars["quality"]
     postgres_conn = BaseHook.get_connection("airflow_postgres_conn")
     postgres_host = postgres_conn.host
     postgres_port = postgres_conn.port
@@ -49,28 +41,23 @@ def get_airflow_config():
     postgres_user = postgres_conn.login
     postgres_password = postgres_conn.password
     postgres_sslmode = postgres_conn.extra_dejson.get("sslmode", "prefer")
+    storage["minio_endpoint"] = minio_endpoint
+    storage["access_key"] = minio_access_key
+    storage["secret_key"] = minio_secret_key
+    general_vars["database"] = {
+        "host": postgres_host,
+        "port": postgres_port,
+        "database": postgres_database,
+        "user": postgres_user,
+        "password": postgres_password,
+        "sslmode": postgres_sslmode,
+    }
     general_config = {
-        "RAW_BUCKET": raw_bucket,
-        "TRUSTED_BUCKET": trusted_bucket,
-        "APP_FOLDER": app_folder,
-        "GTFS_FOLDER": gtfs_folder,
-        "POSITIONS_TABLE_NAME": positions_table_name,
-        "TRIP_DETAILS_TABLE_NAME": trip_details_table_name,
-        "MINIO_ENDPOINT": minio_endpoint,
-        "ACCESS_KEY": minio_access_key,
-        "SECRET_KEY": minio_secret_key,
-        "RAW_DATA_COMPRESSION": raw_data_compression,
-        "RAW_DATA_COMPRESSION_EXTENSION": raw_data_compression_extension,
-        "RAW_EVENTS_TABLE_NAME": raw_events_table_name,
-        "METADATA_BUCKET": metadata_bucket,
-        "QUALITY_REPORT_FOLDER": quality_report_folder,
-        "QUARANTINED_BUCKET": quarantined_bucket,
-        "DB_HOST": postgres_host,
-        "DB_PORT": postgres_port,
-        "DB_DATABASE": postgres_database,
-        "DB_USER": postgres_user,
-        "DB_PASSWORD": postgres_password,
-        "DB_SSLMODE": postgres_sslmode,
+        "storage": storage,
+        "tables": tables,
+        "compression": compression,
+        "quality": quality,
+        "database": general_vars["database"],
     }
     return {
         "general": general_config,
