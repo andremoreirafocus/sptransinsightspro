aecd0fb85d48d312a8cd338cdae3342b5c30d0d1
andremoreirafocus <andre.moreira@focus-sys.com.br>
2026-04-02 15:41:03 -0300

    refactor(refinedsynctripdetails): migrated to use infra modules with interfaces decoupled from config object

--- dags-dev/refinedsynctripdetails/services/load_trip_details_from_storage_to_dataframe.py
+++ dags-dev/refinedsynctripdetails/services/load_trip_details_from_storage_to_dataframe.py
@@ -1,4 +1,4 @@
-from infra.duck_db import get_duckdb_connection
+from infra.duck_db_v2 import get_duckdb_connection
 import logging
 
 logger = logging.getLogger(__name__)
@@ -12,17 +12,23 @@ def load_trip_details_from_storage_to_dataframe(config):
             trip_details_table_name = config["TRIP_DETAILS_TABLE_NAME"]
             if len(trip_details_table_name.split(".")) == 2:
                 trip_details_table_name = trip_details_table_name.split(".")[1]
-            return bucket_name, gtfs_folder, trip_details_table_name
+            connection = {
+                "minio_endpoint": config["MINIO_ENDPOINT"],
+                "access_key": config["ACCESS_KEY"],
+                "secret_key": config["SECRET_KEY"],
+                "secure": False,
+            }
+            return connection, bucket_name, gtfs_folder, trip_details_table_name
         except KeyError as e:
             logger.error(f"Missing required configuration key: {e}")
             raise
 
-    bucket_name, gtfs_folder, trip_details_table_name = get_config(config)
+    connection, bucket_name, gtfs_folder, trip_details_table_name = get_config(config)
     s3_path = f"s3://{bucket_name}/{gtfs_folder}/{trip_details_table_name}/{trip_details_table_name}.parquet"
     logger.info(f"Loading trip_details from storage: {s3_path}")
     con = None
     try:
-        con = get_duckdb_connection(config)
+        con = get_duckdb_connection(connection)
         df = con.execute(f"SELECT * FROM read_parquet('{s3_path}')").df()
         logger.info(f"Successfully loaded {df.shape[0]} trips from storage.")
         return df
