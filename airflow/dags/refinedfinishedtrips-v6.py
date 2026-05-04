ad119789874de3384a1110761753b999194ac922
Andre Moreira <andre.moreira@focus-sys.com.br>
2026-05-04 11:36:54 -0300

    feat(refinedfinishedtrips): moved pipeline invokation to airlfow dataset emitted by transformlivedata

--- airflow/dags/refinedfinishedtrips-v6.py
+++ airflow/dags/refinedfinishedtrips-v6.py
@@ -1,13 +1,14 @@
-import logging
+from refinedfinishedtrips.extract_trips import (
+    extract_trips_for_all_Lines_and_vehicles,
+)
 import os
+import logging
 
-from transformlivedata.transformlivedata import load_transform_save_positions
-
-PIPELINE_NAME = "transformlivedata"
+PIPELINE_NAME = "refinedfinishedtrips"
 _IN_AIRFLOW = bool(os.getenv("AIRFLOW_HOME"))
 
 if _IN_AIRFLOW:
-    VERSION = 9
+    VERSION = 6
     DAG_NAME = f"{PIPELINE_NAME}-v{VERSION}"
 else:
     from logging.handlers import RotatingFileHandler
@@ -25,50 +26,41 @@ else:
 logger = logging.getLogger(__name__)
 
 
-def transform_positions(logical_date_string: str) -> None:
-    load_transform_save_positions(PIPELINE_NAME, logical_date_string)
+def extract_trips():
+    extract_trips_for_all_Lines_and_vehicles(PIPELINE_NAME)
 
 
 if _IN_AIRFLOW:
-    from airflow import DAG
+    from airflow import DAG, Dataset
     from airflow.operators.python import PythonOperator
     from airflow.utils.dates import days_ago
 
+    TRANSFORMED_POSITIONS_READY_SIGNAL = Dataset("sptrans://trusted/transformed_positions_ready")
     default_args = {
         "owner": "airflow",
         "depends_on_past": False,
-        "start_date": days_ago(20),
+        "start_date": days_ago(0),
         "max_active_runs": 1,
         "email_on_failure": False,
         "email_on_retry": False,
-        "retries": 0,
+        "retries": 3,
     }
-
-    def _transform_positions_task(**context):
-        logical_date = context["dag_run"].logical_date
-        logical_date_string = logical_date.isoformat()
-        transform_positions(logical_date_string)
 
     with DAG(
         DAG_NAME,
         default_args=default_args,
-        description="Load data from raw layer, process it, and store it in trusted layer",
-        schedule=None,
+        description="Calculate finished trips for all lines and vehicles",
+        schedule=[TRANSFORMED_POSITIONS_READY_SIGNAL],
         catchup=False,
-        max_active_runs=1,
         tags=["sptrans"],
     ) as dag:
-        transform_positions_task = PythonOperator(
-            task_id="transform_positions",
-            python_callable=_transform_positions_task,
+        extract_trips_task = PythonOperator(
+            task_id="extract_trips", python_callable=extract_trips
         )
-
-        transform_positions_task
+        extract_trips_task
 else:
     def main():
-        logical_date_string = "2026-04-27T15:14:00+00:00"
-        result = transform_positions(logical_date_string)
-        logger.info(f"Pipeline result: {result}")
+        extract_trips()
 
     if __name__ == "__main__":
         main()
