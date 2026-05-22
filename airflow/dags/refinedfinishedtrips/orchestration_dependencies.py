b62cabcf89e48b0730812c72dd2abe1c26d4c6e4
andremoreirafocus <andre.moreira@focus-sys.com.br>
2026-05-22 14:46:16 -0300

    chore(airflow): promoted refinedfinishetrips

--- airflow/dags/refinedfinishedtrips/orchestration_dependencies.py
+++ airflow/dags/refinedfinishedtrips/orchestration_dependencies.py
@@ -5,12 +5,10 @@ from pipeline_configurator.config import get_config
 from refinedfinishedtrips.services.create_quality_report import (
     create_failure_quality_report,
     create_final_quality_report,
-    create_quality_report,
 )
 from refinedfinishedtrips.services.get_all_finished_trips import get_all_finished_trips
 from refinedfinishedtrips.services.get_recent_positions import get_recent_positions
 from refinedfinishedtrips.services.save_finished_trips_to_db import save_finished_trips_to_db
-from refinedfinishedtrips.services.validate_persistence_quality import validate_persistence_quality
 from refinedfinishedtrips.services.validate_positions_quality import validate_positions_quality
 from refinedfinishedtrips.services.validate_trips_quality import validate_trips_quality
 
@@ -22,9 +20,7 @@ class RefinedFinishedTripsOrchestrationDependencies:
     get_all_finished_trips: Callable
     validate_positions_quality: Callable
     validate_trips_quality: Callable
-    validate_persistence_quality: Callable
     save_finished_trips_to_db: Callable
-    create_quality_report: Callable
     create_failure_quality_report: Callable
     create_final_quality_report: Callable
 
@@ -38,9 +34,7 @@ def get_refinedfinishedtrips_orchestration_dependencies() -> (
         get_all_finished_trips=get_all_finished_trips,
         validate_positions_quality=validate_positions_quality,
         validate_trips_quality=validate_trips_quality,
-        validate_persistence_quality=validate_persistence_quality,
         save_finished_trips_to_db=save_finished_trips_to_db,
-        create_quality_report=create_quality_report,
         create_failure_quality_report=create_failure_quality_report,
         create_final_quality_report=create_final_quality_report,
     )
